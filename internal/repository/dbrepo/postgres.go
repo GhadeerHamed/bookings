0616e08fa78df9d0376725b739e7c1c3bc5e7efe
Ghadeer Hamed <eng.gh.a.h93@gmail.com>
2021-07-10 06:13:04 +0300

    Search availability for all rooms

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -65,3 +65,72 @@ func (m *postgresRepo) InsertRoomRestriction(room_es models.RoomRestriction) err
 
 	return nil
 }
+
+//SearchAvailabilityByDatesByRoomID returns true if availability exists for roomID, and false if no availability exists
+func (m *postgresRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select 
+			count(id)
+		from
+			room_restrictions rr
+		where 
+		    room_id=$1
+			$2 <end_date and $3 > start_date;`
+
+	var numRows int
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+
+	return true, nil
+}
+
+//SearchAvailabilityForAllRooms returns a slice of available rooms if any for given dates range
+func (m *postgresRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var rooms []models.Room
+	query := `SELECT 
+				r.id, r.room_name
+			 FROM
+				rooms r 
+			 WHERE 
+				r.id NOT IN (
+					 SELECT 
+						rr.room_id 
+				  	 FROM 
+						room_restrictions rr 
+					 WHERE 
+						$1 < rr.end_date and $2 > rr.start_date
+				);`
+
+	rows, err := m.DB.QueryContext(ctx, query, start, end)
+	if err != nil {
+		return rooms, err
+	}
+
+	for rows.Next() {
+		var room models.Room
+		err := rows.Scan(&room.ID, &room.RoomName)
+		if err != nil {
+			return rooms, err
+		}
+		rooms = append(rooms, room)
+	}
+
+	if err = rows.Err(); err != nil {
+		return rooms, err
+	}
+
+	return rooms, nil
+}
